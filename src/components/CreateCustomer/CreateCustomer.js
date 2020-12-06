160b9974929cab8919b58211dea49c7e0450ca6e
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-12-05 20:59:20 -0800

    created form for creating customer

--- src/components/CreateCustomer/CreateCustomer.js
+++ src/components/CreateCustomer/CreateCustomer.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react'
 
 class CreateCustomer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      customerName: '',
+      customerAddress: ''
+    }
+  }
+
+  onCustomerNameChange = e => {
+    this.setState({ customerName: e.target.value })
+  }
+
+  onCustomerAddressChange = e => {
+    this.setState({ customerAddress: e.target.value })
+  }
+
+  onSubmit = e => {
+    console.log(`Customer name: ${this.state.customerName}`)
+    console.log(`Customer address: ${this.state.customerAddress}`)
+    this.setState({
+      customerName: '',
+      customerAddress: ''
+    })
+  }
+
   render() {
     return (
-      <div>
-        <p>Welcome to Create Customer Component!!</p>
+      <div style={{ marginTop: 20 }}>
+        <h3>Create New Customer</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Customer Name</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.customerName}
+              onChange={this.onCustomerNameChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Customer Address</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.customerAddress}
+              onChange={this.onCustomerAddressChange}
+            />
+          </div>
+        </form>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={this.onSubmit}
+        >
+          Submit
+        </button>
       </div>
     )
   }
