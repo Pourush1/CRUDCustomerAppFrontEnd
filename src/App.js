007d86f0bcec7658c157be757d6684121047a49e
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-12-05 17:50:02 -0800

    added NotFoundPage component to handle the case whenever the url is not correct

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// import HomePage from './pages/HomePage/HomePage'
+import NotFoundPage from './components/NotFoundPage/NotFoundPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          {/* <Route exact path="/" component={HomePage} /> */}
+          <Route path="*" component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
